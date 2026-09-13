e4aa5151fb84769a8859b7c39ff85d16425125ab
윤석민 <yunsukmin522@gmail.com>
2026-09-13 18:49:16 +0900

    1차수정

--- src/dto.py
+++ src/dto.py
@@ -1,9 +1,37 @@
 from pydantic import BaseModel, field_validator
+import re
 
 class CreateUserRequest(BaseModel):
-    # Todo
-    pass
+    name: str 
+    phone_number: str
+    height: float
+    bio: str | None = None
+    
+    @field_validator('phone_number', mode='before')
+    @classmethod
+    def check_phonenum(cls, v: str) -> str:
+        if isinstance(v, str) and re.fullmatch(r"010-\d{4}-\d{4}", v):
+            return v
+        raise ValueError(f"입력하신 전화번호 {v} 는 010-XXXX-XXXX 형식에 맞지 않습니다.")
+
+    @field_validator("height", mode="before")
+    @classmethod
+    def check_height_type(cls, v: object) -> object:
+        if isinstance(v, str):
+            raise ValueError("height는 숫자여야 합니다.")
+        return v
+
+    @field_validator("bio")
+    @classmethod
+    def check_bio_length(cls, v: str | None) -> str | None:
+        if v is not None and len(v) > 500:
+            raise ValueError("bio는 500자를 초과할 수 없습니다.")
+        return v
+
 
 class UserResponse(BaseModel):
-    # Todo
-    pass
+    user_id: int
+    name: str
+    phone_number: str
+    height: float
+    bio: str | None = None
--- src/main.py
+++ src/main.py
@@ -9,26 +9,55 @@ user_db = {}
 
 @app.post("/api/users")
 def create_user(request: CreateUserRequest) -> UserResponse:
-    # Todo
+    user_id = len(user_db)+1
+    user_db[user_id] = request
 
-    return 0
+    return UserResponse(
+        user_id=user_id,
+        name=request.name,
+        phone_number=request.phone_number,
+        height=request.height,
+        bio = request.bio
+    )
 
 
 @app.get("/api/users/{user_id}")
 def get_user(
-    # Todo
-    
+    user_id: int = fastapi.Path(
+        ...,
+        ge=1
+    ),
 ) -> UserResponse:
-    # Todo
-
-    return 0
+    if user_id not in user_db:
+        raise ValueError("Error : the user id is not existed.")
+    
+    request = user_db[user_id]
+    return UserResponse(
+        user_id=user_id, 
+        name=request.name,
+        phone_number=request.phone_number,
+        height=request.height,
+        bio = f"{request.bio} 컴퓨터공학과에 재학중인 {request.name}입니다."
+    )
 
 
 @app.get("/api/users")
 def get_users(
-    # Todo
-
+    min_height: float,
+    max_height: float
 ) -> list[UserResponse]:
-    # Todo
+    requestList = []
 
-    return 0
+    for i, user in user_db.items():
+        user = user_db[i]
+        if min_height <= user.height <= max_height:
+            userDTO = UserResponse(
+                user_id=i,
+                name=user.name,
+                phone_number=user.phone_number,
+                height=user.height,
+                bio=user.bio
+                )
+            requestList.append(userDTO)
+    
+    return requestList
